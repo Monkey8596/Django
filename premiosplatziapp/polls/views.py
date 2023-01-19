c8981868baf11c16db728c1140c2ec381a5652e3
Sayin <luffyp2001@gmail.com>
2023-01-19 14:22:08 -0500

    Views

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,21 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 def index(request):
-    return HttpResponse('Hello World')
+    return HttpResponse('You are in the Home Page Platzi Awards')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'This is the question number {question_id} ')
+
+
+def results(request, question_id):
+    return HttpResponse(f'This is the results of the question number {question_id} ')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting for the question number {question_id} ')
 
