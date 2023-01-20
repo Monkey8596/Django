29c48a796f662686767e62682e6f07eee1f2cc86
Sayin <luffyp2001@gmail.com>
2023-01-19 22:12:11 -0500

    template

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -2,10 +2,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+from .models import Question
 
 def index(request):
-    return HttpResponse('You are in the Home Page Platzi Awards')
+    latest_question_list = Question.objects.all()
+    return render(request, 'polls/index.html', {
+        'latest_question_list': latest_question_list
+    } )
 
 
 def detail(request, question_id):
