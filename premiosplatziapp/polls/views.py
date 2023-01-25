d3192ddae4bb0f7224af21d492865742587d0a2a
Sayin <luffyp2001@gmail.com>
2023-01-24 20:57:39 -0500

    Template Detail

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Question
@@ -12,7 +12,10 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f'This is the question number {question_id} ')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request,"polls/detail.html",{
+        'question': question
+    })
 
 
 def results(request, question_id):
