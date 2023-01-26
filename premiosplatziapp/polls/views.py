417297246270078a4badebbcb28dd3228476b12c
Sayin <luffyp2001@gmail.com>
2023-01-26 16:59:44 -0500

    Vote

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,8 +1,9 @@
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.all()
@@ -23,5 +24,15 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse(f'You are voting for the question number {question_id} ')
-
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist ):
+        return render(request, 'polls/detail.html',{
+            'question': question,
+            'error_message': 'You did not selected any answer'
+        } )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
