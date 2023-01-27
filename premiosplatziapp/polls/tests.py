770a34a8e69237ae918e2a17342cf7b0f3e2c0a0
Sayin <luffyp2001@gmail.com>
2023-01-27 11:48:59 -0500

    Test

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+# Models
+# Views
+class QuestionModelTests(TestCase):
+
+    def  test_was_published_recently_with_future_questions(self):
+        
+        '''
+        was_published_recently returns False for the questions whose pub_date is inn the future
+        '''
+
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question( question_text='Whos is the best Course Director on Platzi' , pub_date = time )
+        self.assertIs(future_question.was_published_recently(), False)
